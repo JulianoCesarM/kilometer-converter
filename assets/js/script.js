ffa67be98544319f75f587c87c43e8b8461a1319
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-11-22 11:53:10 -0300

    creating functions and adding error messages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,27 @@
 function converterDistance() {
   const distanceLightYear = document.querySelector("#distanceLightYear").value
   const distanceName = document.querySelector("#distanceName").value
-  let calculo // 1 ano-luz 9,46 trilhoes de Km
 
-  const resp = document.querySelector("p#resp")
+  if (distanceLightYear == 0) return alert("O input não pode ser vazio ou 0!")
 
   if (distanceName == "quilometros") {
-    calculo = 9.461e12
-    const distanceQuilometers = distanceLightYear * calculo
-    msg = `${distanceLightYear} anos-luz está a ${distanceQuilometers} quilometros de distancia`
-    resp.classList.add("resp")
-    return (resp.innerText = msg)
+    const distanciaKM = 9.461e12
+    const calculoDistance = calculo(distanceLightYear, distanciaKM)
+    return respostaNaTela(distanceLightYear, distanceName, calculoDistance)
   }
   if (distanceName == "metros") {
-    calculo = 9.461e15
-    const distanceMeters = distanceLightYear * calculo
-    msg = `${distanceLightYear} anos-luz está a ${distanceMeters} quilometros de distancia`
-    resp.classList.add("resp")
-    return (resp.innerText = msg)
+    const distanciaM = 9.461e15
+    const calculoDistance = calculo(distanceLightYear, distanciaM)
+    return respostaNaTela(distanceLightYear, distanceName, calculoDistance)
   }
+  return alert("O input não pode ser vazio ou o valor está errad0!")
+}
+function calculo(m, n) {
+  return m * n
+}
+function respostaNaTela(distanceLightYear, distanceName, calculoDistance) {
+  const resp = document.querySelector("#resp")
+  const msg = `${distanceLightYear} anos-luz está a ${calculoDistance} ${distanceName} de distancia!`
+  resp.classList.add("resp")
+  return (resp.innerText = msg)
 }
